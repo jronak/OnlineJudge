9711b6a0f4572c1830691df1a48e099f17dbb1b6
Vamsi Sai T <vamsi@vamsi-laptop.com>
2015-07-16 12:07:56 +0530

    Added db.sql/Changed routes

--- routers/router.go
+++ routers/router.go
@@ -6,11 +6,16 @@ import (
 )
 
 func init() {
-	//Exec is running at the root
-	beego.Router("/", &controllers.ExecController{}, "*:Get;post:Post")
-	beego.Router("/submit", &controllers.ExecController{}, "post:Submit;*:Get")
-	beego.Router("/login", &controllers.LoginController{})
-	beego.Router("/problem", &controllers.ProblemController{})
+	// List problems (or have a seperate controller as IndexController?)
+	beego.Router("/", &controllers.ProblemController{}, "*:List")
+
+	// On User -- Includes user profiles, settings and login and sign up routes
+	beego.AutoRouter(&controllers.UserController{})
+
+	// On Problems
+	beego.Router("/problem/create", &controllers.ProblemController{}, "*:Create;post:SaveProblem")
 	beego.Router("/problem/:type", &controllers.ProblemController{}, "*:ProblemsByCategory")
-	beego.Router("/problem/:type/:name", &controllers.ProblemController{}, "*:ProblemByStatement")
+	beego.Router("/problem/:type/:id", &controllers.ProblemController{}, "*:ProblemByStatement")
+	beego.Router("/problem/:type/:id/submit", &controllers.ProblemController{}, "post:SaveSubmission;*:Submit") // ->ProblemController(notes that user has tried solving problem)->ExecController(seek for helper to exec)->ProblemController(get result info & build on it)
+	beego.Router("/problem/:type/:id/edit", &controllers.ProblemController{}, "post:SaveProblem;*:Edit")
 }
